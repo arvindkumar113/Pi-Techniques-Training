def438f6d96bcccbe4aa8b44c3e8d02cb6c5cf9e
arvind.sahu <aksahuboss@gmail.com>
2021-07-21 14:12:46 +0530

    more files added

---
+++ Indexers_In_CSharp/Indexers_In_CSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_In_CSharp
+{
+    public class User
+    {
+        //decraring an array of strings
+        private string[] userName = new string[5];
+
+        public string this[int i]
+        {
+            get { return userName[i]; }
+            set { userName[i] = value; }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //assigining username using index
+            User userList = new User();
+            userList[0] = "Arvind Kumar";
+            userList[1] = "Shubham Kumar";
+            userList[2] = "Pranay";
+            userList[3] = "Manthan Charausia";
+
+            Console.WriteLine("Printing User class object using index");
+            for(int i=0; i<4;i++)
+            {
+                Console.WriteLine(userList[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}

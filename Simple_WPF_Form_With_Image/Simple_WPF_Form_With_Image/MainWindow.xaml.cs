c55b1f5409ece9262fe7b089ceed3697927b7aaf
arvind.sahu <aksahuboss@gmail.com>
2021-07-22 18:51:29 +0530

    files added

---
+++ Simple_WPF_Form_With_Image/Simple_WPF_Form_With_Image/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using Microsoft.Win32;
+
+namespace Simple_WPF_Form_With_Image
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["connstr"].ConnectionString);
+        SqlCommand command;
+        string imgLoc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void buttonBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dlg = new OpenFileDialog();
+                dlg.Filter = "JPG Files (*.jpg)|*.jpg|GIF Files (*.gif)|*.gif | All Files(*.*)|*.*";
+                dlg.Title = "Select epmployee Image";
+                dlg.ShowDialog();
+                {
+                    imgLoc = dlg.FileName;
+                    imageBoxEmp.Source = imgLoc;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button_Show_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TreeViewToDisplayAllProductCategoryData/TreeViewToDisplayAllProductCategoryData/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeViewToDisplayAllProductCategoryData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                // Bind The RelationShip Data
+                this.ProductTreeView.DataContext = NorthWind.CreateDataRelation();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
